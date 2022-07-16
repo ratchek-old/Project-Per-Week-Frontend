dee2cc9172a911a8f0060559694aec43604aaf06
ratchek <git@ratchek.com>
2022-07-16 10:38:13 -0400

    Add highlighting to today's date

--- 02-dont-break-the-chain/app/src/Day.js
+++ 02-dont-break-the-chain/app/src/Day.js
@@ -1,6 +1,13 @@
 const Day = (props) => {
+  const isToday = (date) => { return date.toDateString() === new Date().toDateString();}
   return (
-    <div className="day"> {props.date.getDate()} </div>
+    <div 
+      className="day"
+      id={isToday(props.date) ?"today": null}
+      key = {props.date.getDate()}
+    > 
+        {props.date.getDate()} 
+    </div>
   );
 }
 
