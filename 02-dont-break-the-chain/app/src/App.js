62609052257580d5e5a2c67e9a0914da6da63a74
ratchek <git@ratchek.com>
2022-07-16 09:13:23 -0400

    Add 'addWeeks' function to help with building the calendar

--- 02-dont-break-the-chain/app/src/App.js
+++ 02-dont-break-the-chain/app/src/App.js
@@ -1,5 +1,5 @@
 import Week from "./Week"
-
+import { addWeeks } from "./TimeTravel";
 function App() {
   const date = new Date();
 
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
         <h1> Hello world!</h1>
         <Week date={date}/>
+        <Week date={addWeeks(date, 1)} />
     </div>
   );
 }
--- 02-dont-break-the-chain/app/src/TimeTravel.js
+++ 02-dont-break-the-chain/app/src/TimeTravel.js
@@ -3,3 +3,10 @@ export function addDays(date, numberOfDays){
     newDate.setDate(date.getDate() + numberOfDays);
     return (newDate);
   }
+
+export function addWeeks(date, numberOfWeeks){
+let newDate = new Date();
+newDate.setDate(date.getDate() + 7 * numberOfWeeks);
+return (newDate);
+}
+
