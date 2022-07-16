24304e95952da85a4516d5584b118fb0f2d0334d
ratchek <git@ratchek.com>
2022-07-16 15:54:28 -0400

    Add toggleable 'success' class

--- 02-dont-break-the-chain/app/src/Day.js
+++ 02-dont-break-the-chain/app/src/Day.js
@@ -1,14 +1,26 @@
 import { isEvenMonth } from "./TimeTravel";
+import {useState} from 'react';
 
 const Day = (props) => {
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  function clickHandler() {
+    if (isSuccess) {setIsSuccess(false)}
+    else {setIsSuccess(true)}
+  }
 
   const isToday = (date) => { return date.toDateString() === new Date().toDateString();}
+  let classes = "";
+  classes += " day";
+  classes += isEvenMonth(props.date) ? " even-month" : " odd-month";
+  classes += isSuccess ? " success" : "";
 
   return (
     <div 
-      className= {isEvenMonth(props.date) ? "day even-month" : "day odd-month"}
+      className= {classes}
       id={isToday(props.date) ?"today": null}
       key = {props.date.getDate()}
+      onClick = {clickHandler}
     > 
         {props.date.getMonth() + " / "  + props.date.getDate()} 
     </div>
